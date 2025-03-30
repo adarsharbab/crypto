672d3ba63988cda282eb74b26a93031d555f6cbc
ADARSH ARBAB <33250346+adarsharbab@users.noreply.github.com>
2025-03-30 10:15:24 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  base: "/crypto/",  // ✅ Set this to match your repo name
 });
